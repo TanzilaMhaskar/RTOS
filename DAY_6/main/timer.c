63b7b26acdbfb5bac0b6142d8222a6a5509d12b6
TanzilaMhaskar <tanzila13mhaskar@gmail.com>
2022-01-22 08:01:29 +0530

    Timer

---
+++ DAY_6/main/timer.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <freertos/timers.h>
+
+TimerHandle_t motor_timer_handle;
+
+
+void TurnMotorOFF(TimerHandle_t xTimer)
+{
+printf("Turning motor off\n");
+}
+
+void  app_main()
+{
+    printf("Hello Free RTOS\n");
+    motor_timer_handle= xTimerCreate("MotorOFF",pdMS_TO_TICKS(2000),pdTRUE,NULL,TurnMotorOFF);
+    
+    xTimerStart(motor_timer_handle,0);
+    while(1)
+    {
+    printf("Main Task\n");
+    vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+    
+}
